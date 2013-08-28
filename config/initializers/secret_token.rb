cf0fe89dbf4d4a52a8adeec83fcb6185277ebe1b
andrew flaherty-moran <andrw.flaherty@gmail.com>
2013-08-27 21:58:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lintbury::Application.config.secret_token = '76c708c4c1cafec3fc3914a8cb7314f76656e48e2961fdf1013bff5a4e24b0c272d9c93ebf6044b52f8c1e3de9f75de8f63f937cfc185c19100a32d996b14d5e'
